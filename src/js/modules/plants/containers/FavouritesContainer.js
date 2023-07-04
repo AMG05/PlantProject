38a6ea89405bc5250251417e631836d50d870525
Anne McGinness <anne.mcginness@stormid.com>
2023-07-04 12:41:33 +0100

    favourites changes

--- src/js/modules/plants/containers/FavouritesContainer.js
+++ src/js/modules/plants/containers/FavouritesContainer.js
@@ -22,9 +22,9 @@ const FavList = () => {
     }, []);
 
     return (
-    <div>
+    <div className="list-plants">
         <ul>{favourites.map(plant => (
-            <li key={plant.id}>{plant.common_name}</li>))}
+            <li key={plant.id}><h2>{plant.common_name}</h2><h3>How to care for me:</h3> <p>{plant.watering} Watering</p><h3>Where I thrive:</h3> <p>{plant.sunlight}</p></li>))}
             </ul>
     </div>
     );
